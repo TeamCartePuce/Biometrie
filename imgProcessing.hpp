9fba6d6a0514fd221cb2ebde507c1b9ced8c8c12
Florian .C <cathelineauflorian@gmail.com>
2018-01-09 15:07:24 +0100

    init img processing tools file

---
+++ imgProcessing.hpp
@@ -0,0 +1,19 @@
+/*
+ * imgProcessing.cpp
+ *
+ *  Created on: 9 janv. 2018
+ *      Author: ochtar
+ */
+
+#ifndef IMGPROCESSING_HPP_
+#define IMGPROCESSING_HPP_
+
+#include <opencv2/opencv.hpp>
+
+namespace cv {
+
+	class imgProcessing{
+	};
+};
+
+#endif /* IMGPROCESSING_HPP_ */
