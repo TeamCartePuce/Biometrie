2f05c08e982b6c4341c5373c1bf23bb7cba84458
Florian .C <cathelineauflorian@gmail.com>
2018-01-09 15:19:04 +0100

    Add image read

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	Mat image;
-	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+	image = imread("res/ellipse_blanche.png", CV_LOAD_IMAGE_COLOR);   // Read the file
 
 	if(! image.data )                              // Check for invalid input
 	{
